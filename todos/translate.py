76b120b3b5c084e3fa0139f1cc8801e48950423a
EC2 Default User <ec2-user@ip-172-31-89-122.ec2.internal>
2021-06-08 18:36:12 +0000

    F: Typo error!

--- todos/translate.py
+++ todos/translate.py
@@ -42,7 +42,7 @@ def get(event, context):
 
 
 def detect_language(text):
-    return comprehend.detect_dominant_language(text='string')
+    return comprehend.detect_dominant_language(Text='string')
 
     
 def translate_text(text, source, target):
