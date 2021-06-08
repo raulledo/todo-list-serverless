f83f1b4e7107fd7b7a6463168d8d940aba009fc0
EC2 Default User <ec2-user@ip-172-31-89-122.ec2.internal>
2021-06-08 00:14:03 +0000

    N: New translate service

---
+++ todos/translate.py
@@ -0,0 +1,50 @@
+# change: test cd/ci
+import os
+import json
+import com.amazonaws.services.comprehend
+
+from todos import decimalencoder
+import boto3
+dynamodb = boto3.resource('dynamodb')
+translate = boto3.client('translate')
+comprehend = boto3.client('comprehend')
+
+
+def get(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+    
+    # set target language
+    target = event['pathParameters']['id']
+    
+    # set text
+    text = result['Item']['text']
+    
+    # detect source language
+    source_language = detect_language(text);
+    
+    # translate text
+    result['Item']['text'] = translate_text(text, source_language, target)['TranslateText']
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Item'],
+                           cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
+
+
+def detect_language(text):
+    return comprehend.detect_dominant_language(text='string')
+
+    
+def translate_text(text, source, target):
+    return translate.traslate_text(Text=text, SourceLanguageCode=source, TargetLanguageCode=target)
